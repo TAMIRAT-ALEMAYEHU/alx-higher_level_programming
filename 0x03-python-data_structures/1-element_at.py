09095eea100f5cc25ab6cf2be8cf924c458e26c6
Tamirat <88383918+TAMIRAT-ALEMAYEHU@users.noreply.github.com>
2021-11-21 23:11:34 +0300

    edted

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-digit = abs(number) % 10
-if number < 0:
-    digit = -digit
-print("Last digit of {} is {} and is ".format(number, digit), end="")
-if digit > 5:
-    print("greater than 5")
-elif digit == 0:
-    print("0")
-else:
-    print("less than 6 and not 0")
+# 1-element_at.py
+
+
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
